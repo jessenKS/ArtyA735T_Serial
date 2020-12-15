94a7abb9c7ddb2c8054edee3a631ed630377aa76
jessenKS <54026475+jessenKS@users.noreply.github.com>
2020-12-15 12:54:09 -0300

    Change server

--- uart.py
+++ uart.py
@@ -1,8 +1,10 @@
 import serial
 
+print("Iniciando Serial...")
 #use ttyUSB0 se for o caso, ou se for Windows, COMx.
 fpga = serial.Serial('COM4',9600)
+print("Serial Iniciada")
 
 while 1:
-    teste = fpga.read(20)
-    print(teste)
+    serialRead = fpga.read(20)
+    print("Leitura: " + str(serialRead))
